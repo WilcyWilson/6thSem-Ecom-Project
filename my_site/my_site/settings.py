edbd25beda0498cdd6a283c3836d7fe3ab87330f
wilcywilson <wilsonshresthaapps@gmail.com>
2021-09-08 09:07:58 +0545

    currency

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -130,7 +130,7 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
